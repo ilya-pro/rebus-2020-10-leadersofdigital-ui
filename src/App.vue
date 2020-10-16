5ad8d2f47bd6857d9a82e4c0d46dfde032476d49
Ilya Pro <ilya.pro.dev@gmail.com>
2020-10-17 01:39:35 +0300

    login form base routes eslint fix

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,53 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <!-- navigation drawer -->
+    <Drawer v-model="drawer"
+            :items="items"/>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <!-- header -->
+    <v-app-bar app
+               color="#262C40"
+               dark>
     </v-app-bar>
-
+    <!-- Sizes your content based upon application components -->
     <v-main>
-      <HelloWorld/>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+        <!-- view for current route -->
+        hello
+        <router-view></router-view>
+      </v-container>
     </v-main>
+
+    <v-footer app>
+      <!--{{ $route.name }}-->
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+// import HelloWorld from './components/HelloWorld'
+import Drawer from '@/components/Drawer'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld
+    Drawer
   },
 
   data: () => ({
-    //
+    drawer: false,
+    // пункты навигаии
+    items: [
+      /*{id: 'home', title: 'Главная', icon: 'mdi-star', path: '/'},*/
+      {id: 'modules', title: 'Учебные модули', icon: 'mdi-lightbulb', path: '/modules'},
+      /*{id: 'account', title: 'Аккаунт', icon: 'mdi-account', path: '/account'}*/
+      {id: 'projects', title: 'Проекты', icon: 'mdi-star', path: '/projects'},
+      {id: 'shop', title: 'Магазин', icon: 'mdi-star', path: '/shop'},
+    ],
   })
 }
 </script>
